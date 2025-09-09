f5794dd07c488c710b04b227cb4f819f0b1cb5db
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-09-09 15:56:18 -0700

    Avoid Wunused warning for test

--- tests/arena_test.c
+++ tests/arena_test.c
@@ -18,17 +18,20 @@ void test_arena_basic() {
 
     // Allocate 16 bytes
     void *ptr1 = arena_alloc(&arena, 16);
+    (void)ptr1;
     assert(ptr1 != NULL);
     assert(is_aligned(ptr1, sizeof(max_align_t_c99)));
 
     // Allocate another 16 bytes
     void *ptr2 = arena_alloc(&arena, 16);
+    (void)ptr2;
     assert(ptr2 != NULL);
     assert(is_aligned(ptr2, sizeof(max_align_t_c99)));
     assert(ptr2 > ptr1);
 
     // Allocate chunk that exceeds capacity of first arena (to test linked list allocation)
     void *ptr3 = arena_alloc(&arena, capacity * 2);
+    (void)ptr3;
     assert(ptr3 != NULL);
     assert(is_aligned(ptr3, sizeof(max_align_t_c99)));
     assert(arena.next != NULL);
@@ -40,6 +43,7 @@ void test_arena_basic() {
 
     // Allocate again after reset
     void *ptr4 = arena_alloc(&arena, 8);
+    (void)ptr4;
     assert(ptr4 != NULL);
     assert(is_aligned(ptr4, sizeof(max_align_t_c99)));
 
